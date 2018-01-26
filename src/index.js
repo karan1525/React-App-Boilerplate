880899f0e293085cd8673d878c015c000cf27e71
Karan Bhargava <karan1525@yahoo.co.in>
2018-01-25 22:58:14 -0800

    Pushed complete react-app-boilerplate for SCSS and SASS styling

---
+++ src/index.js
@@ -0,0 +1,10 @@
+// BASE BOILERPLATE FOR REACT
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './styles/styles.scss';
+
+ReactDOM.render(
+  <p>This is a boilerplate for SCSS and SASS</p>,
+  document.querySelector('#root')
+);
